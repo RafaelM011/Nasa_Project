13fbc6ea94457ee4821644baf2b415bcd6887475
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-08-05 20:22:48 -0400

    Misc changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 import 'dotenv/config';
+import mongoose from 'mongoose';
 import http from "http";
 import app from "./src/app.js";
 
 global.__dirname = import.meta.dirname;
 
 const PORT = process.env.PORT || 4000;
-const MONGO_URL = "mongodb+srv://rafaelmartinez011dev:Julio11@nasa-project.hcuhty9.mongodb.net/?retryWrites=true&w=majority&appName=Nasa-Project";
+const MONGO_URL = "mongodb+srv://rafaelmartinez011dev:Julio11@nasa-project.hcuhty9.mongodb.net/nasa_project?retryWrites=true&w=majority&appName=Nasa-Project";
 
-http.createServer(app).listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
+mongoose.connection.once('open', () => console.log('MongoDb connection ready~!'));
+mongoose.connection.on('error', console.error)  ;
+
+async function startServer(){
+  await mongoose.connect(MONGO_URL);
+
+  http.createServer(app).listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
+}
+
+startServer();
