0f19bc9689925926b7c14ce1b0a03ebd21055e23
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-08-05 20:22:42 -0400

    Updated controllers to use MongoDb

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,20 +1,20 @@
-import { getAllLaunches, addLaunch, deleteLaunch } from "#models/launches.model..js";
+import { getAllLaunches, addLaunch, deleteLaunch } from "#models/launches.model.js";
 
-export function httpGetAllLaunches(_, res) {
-  return res.status(200).json(getAllLaunches());
+export async function httpGetAllLaunches(_, res) {
+  return res.status(200).json(await getAllLaunches());
 };
 
-export function httpSubmitLaunch(req, res){
+export async function httpSubmitLaunch(req, res){
   const launch = req.body;
   launch.launchDate = new Date(launch.launchDate);
-  addLaunch(launch);
+  await addLaunch(launch);
 
   return res.status(201).json({data: launch, success: true});
 }
 
-export function httpDeleteLaunch(req, res){
+export async function httpDeleteLaunch(req, res){
   const id = req.params.id;
-  const deleted = deleteLaunch(id);
+  const deleted = await deleteLaunch(id);
 
   if(deleted) return res.status(200).json({data: id, success: true});
   return res.status(400).json({data: id, success: false});
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,5 +1,5 @@
 import { getAllPlanets } from "#models/planets.model.js";
 
-export function httpGetAllPlanets(_, res) {
-  return res.status(200).json(getAllPlanets());
+export async function httpGetAllPlanets(_, res) {
+  return res.status(200).json(await getAllPlanets());
 };
