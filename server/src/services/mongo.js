84fa8cb11c33df137e84b118e09888a034e0d782
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-08-24 21:54:31 -0400

    Removing unnecesary code

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-// const MONGO_URL = `mongodb+srv://${process.env.MONGO_ACCOUNT}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_0}.hcuhty9.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=${process.env.MONGO_CLUSTER}`;
-
 mongoose.connection.once('open', () => console.log('MongoDb connection ready~!'));
 mongoose.connection.on('error', console.error);
 
