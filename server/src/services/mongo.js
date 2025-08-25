d437dccfc946cc70d1ecf6c06619b797f7cc0600
Rafael Martinez <rafaelmartinez011.dev@gmail.com>
2025-08-24 21:51:44 -0400

    Setting up MongoDB for GithubActions

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const MONGO_URL = `mongodb+srv://${process.env.MONGO_ACCOUNT}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_0}.hcuhty9.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=${process.env.MONGO_CLUSTER}`;
+// const MONGO_URL = `mongodb+srv://${process.env.MONGO_ACCOUNT}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_0}.hcuhty9.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=${process.env.MONGO_CLUSTER}`;
 
 mongoose.connection.once('open', () => console.log('MongoDb connection ready~!'));
 mongoose.connection.on('error', console.error);
 
-export const connectMongo = async() => await mongoose.connect(MONGO_URL);
+export const connectMongo = async() => await mongoose.connect(process.env.MONGO_URL);
 export const disconnectMongo = async() => await mongoose.disconnect();
